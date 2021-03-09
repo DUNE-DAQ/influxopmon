2040041af950ca86b3977054040ada51a280c780
glehmannmiotto <giovanna.lehmann@cern.ch>
2021-03-09 10:27:51 +0100

    first package commit

---
+++ plugins/influxOpmonService.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <nlohmann/json.hpp>
+
+#include "opmonlib/OpmonService.hpp"
+#include "influxdb.hpp"
+
+namespace dunedaq {
+
+     ERS_DECLARE_ISSUE(influxopmon, CannotPostToDB,
+                     "Cannot post to Influx DB " << name,
+                      ((std::string)name))
+
+}
+
+
+namespace dunedaq::influxopmon {
+
+class influxOpmonService : public OpmonService
+{
+  public: 
+    explicit influxOpmonService(std::string uri) : OpmonService(uri) {
+        // FIXME: Get the DB connection string from the URI.
+        // The URI will be something like influx://db_host:db_port:db_name....
+        m_host = "dbod-testinfluxyd.cern.ch";
+        m_port = 8095;
+        m_dbname = "db1";
+    }
+
+    void publish( nlohmann::json j )
+    {
+
+        // influxdb_cpp::server_info si(m_host, m_port, .....);
+	// FIXME: do here the reformatting of j and the posting to the db
+
+    }
+  protected:
+    typedef OpmonService inherited;
+  private:
+    //std::ofstream m_ofs;
+    std::string m_host;
+    int32_t m_port;
+    std::string m_dbname;
+    // FIXME: add here utility methods
+    
+};
+
+}
+
+extern "C" {
+  std::shared_ptr<dunedaq::opmonlib::OpmonService> make(std::string service) {
+    return std::shared_ptr<dunedaq::opmonlib::OpmonService>(new dunedaq::opmonlib::influxOpmonService(service));
+  }
+}
+
