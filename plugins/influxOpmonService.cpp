def103c2e2a048f5c38f3e594bda518e2595f3f5
Tenkarast <61792464+Tenkarast@users.noreply.github.com>
2021-03-10 10:33:41 +0100

    Clean, compiling package

--- plugins/influxOpmonService.cpp
+++ plugins/influxOpmonService.cpp
@@ -2,7 +2,6 @@
 #include <nlohmann/json.hpp>
 
 #include "opmonlib/OpmonService.hpp"
-#include "JsonInfluxConverter/JsonInfluxConverter.h"
 #include "influxdb.hpp"
 
 namespace dunedaq {
@@ -18,57 +17,31 @@ namespace dunedaq::influxopmon {
 
 class influxOpmonService : public dunedaq::opmonlib::OpmonService
 {
-    public: 
-        explicit influxOpmonService(std::string uri) : OpmonService(uri) 
-        {
-            //std::string uri = "influx://dbod-testinfluxyd.cern.ch:8095:db1:admin:admin";
-            uri = uri.substr(uri.find("/") + 2);
-            std::string tmp;
-            std::stringstream ss(uri);
-            std::vector<std::string> ressource;
-            while (getline(ss, tmp, ':'))
-            {
-                ressource.push_back(tmp);
-            }
+  public: 
+    explicit influxOpmonService(std::string uri) : dunedaq::opmonlib::OpmonService(uri) {
+        // FIXME: Get the DB connection string from the URI.
+        // The URI will be something like influx://db_host:db_port:db_name....
+        m_host = "dbod-testinfluxyd.cern.ch";
+        m_port = 8095;
+        m_dbname = "db1";
+    }
 
-            // FIXME: Get the DB connection string from the URI.
-            // The URI will be something like influx://db_host:db_port:db_name....
-            m_host = ressource[0];
-            m_port = stoi(ressource[1]);
-            m_dbname = ressource[2];
-            m_dbaccount = ressource[3];
-            m_dbpassword = ressource[4];
-        }
+    void publish( nlohmann::json j )
+    {
 
-        void publish( nlohmann::json j )
-        {
+        // influxdb_cpp::server_info si(m_host, m_port, .....);
+	// FIXME: do here the reformatting of j and the posting to the db
 
-            influxdb_cpp::server_info si(m_host, m_port, m_dbname, m_dbaccount, m_dbpassword);
-            //influxdb_cpp::server_info si("dbod-testinfluxyd.cern.ch", 8095, "pyexample", "admin", "admin");
-            std::string resp;
-
-            jsonConverter.setInsertsVector(false, tagSetVector, timeVariableName, gather_info(level));
-            insertsVector = jsonConverter.getInsertsVector();
-
-            for (int i = 0; i < insertsVector.size(); i++)
-            {
-                influxdb_cpp::query(resp, insertsVector[i], si);
-                std::cout << resp << std::endl;
-            }
-        }
-    protected:
-        typedef OpmonService inherited;
-    private:
-        //std::ofstream m_ofs;
-        std::string m_host;
-        int32_t m_port;
-        std::string m_dbname;
-
-        // FIXME: add here utility methods
-        std::vector<std::string> tagSetVector;
-        tagSetVector.push_back(".class_name=");
-        std::string timeVariableName = ".time=";
-        JsonConverter jsonConverter;
+    }
+  protected:
+    typedef OpmonService inherited;
+  private:
+    //std::ofstream m_ofs;
+    std::string m_host;
+    int32_t m_port;
+    std::string m_dbname;
+    // FIXME: add here utility methods
+    
 };
 
 }
