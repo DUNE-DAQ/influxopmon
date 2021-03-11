9f4b11900093e31deec8ed0f5d3b80463d2270f0
Tenkarast <61792464+Tenkarast@users.noreply.github.com>
2021-03-11 15:26:57 +0100

    Update influxOpmonService.cpp

--- plugins/influxOpmonService.cpp
+++ plugins/influxOpmonService.cpp
@@ -4,7 +4,6 @@
 #include "opmonlib/OpmonService.hpp"
 #include "influxdb.hpp"
 
-
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -26,13 +25,30 @@ namespace dunedaq::influxopmon {
     {
     public:
         explicit influxOpmonService(std::string uri) : dunedaq::opmonlib::OpmonService(uri) {
-            // FIXME: Get the DB connection string from the URI.
-            // The URI will be something like influx://db_host:db_port:db_name....
-            m_host = "dbod-testinfluxyd.cern.ch";
-            m_port = 8095;
-            m_dbname = "db1";
+            uri = uri.substr(uri.find("/") + 2);
+            std::string tmp;
+            std::stringstream ss(uri);
+            std::vector<std::string> ressource;
+            while (getline(ss, tmp, ':'))
+            {
+                ressource.push_back(tmp);
+            }
 
-            std::cout << "Hello world";
+            //std::string uri = "influx://dbod-testinfluxyd.cern.ch:8095:db1:admin:admin"
+            m_host = ressource[0];
+            m_port = stoi(ressource[1]);
+            m_dbname = ressource[2];
+            m_dbaccount = ressource[3];
+            m_dbpassword = ressource[4];
+
+            std::cout << "Hello world\n";
+
+
+            influxdb_cpp::server_info si(m_host, m_port, m_dbname, m_dbaccount, m_dbpassword);
+            //influxdb_cpp::server_info si("dbod-testinfluxyd.cern.ch", 8095, "pyexample", "admin", "admin");
+            std::string resp;
+
+            influxdb_cpp::query(resp, "CREATE DATABASE mydbX", si);
         }
 
         void publish(nlohmann::json j)
@@ -49,7 +65,8 @@ namespace dunedaq::influxopmon {
         std::string m_host;
         int32_t m_port;
         std::string m_dbname;
-        // FIXME: add here utility methods
+        std::string m_dbaccount;
+        std::string m_dbpassword;
 
     };
 
