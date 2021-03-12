82721702c1ecbbd61f0664b64382b5d5207450a3
Tenkarast <61792464+Tenkarast@users.noreply.github.com>
2021-03-12 16:11:07 +0100

    Update influxOpmonService.cpp

--- plugins/influxOpmonService.cpp
+++ plugins/influxOpmonService.cpp
@@ -55,24 +55,31 @@ namespace dunedaq::influxopmon {
                 ressource.push_back(tmp);
             }
 
-            //std::string uri = "influx://dbod-testinfluxyd.cern.ch:8095:db1:admin:admin"
+            //std::string uri = "influx://dbod-testinfluxyd.cern.ch:8095:db1:admin:admin:https:.time=:.class_name="
             m_host = ressource[0];
             m_port = ressource[1];
             m_dbname = ressource[2];
             m_dbaccount = ressource[3];
             m_dbpassword = ressource[4];
+            m_postProtocol = ressource[5];
+            m_delimiter = ressource[6];
 
-            tagSetVector.push_back(".class_name=");
+            for (unsigned long int i = 7; i < ressource.size(); i++)
+            {
+                tagSetVector.push_back(ressource[i]);
+            }
+          
+            
         }
 
         void publish(nlohmann::json j)
         {
             
-            jsonConverter.setInsertsVector(false, tagSetVector, timeVariableName, j.flatten().dump());
+            jsonConverter.setInsertsVector(false, tagSetVector, m_delimiter, j.flatten().dump());
             insertsVector = jsonConverter.getInsertsVector();
             
             
-            querry = "curl -i -XPOST 'https://" + m_host + ":" + m_port + "/write?db=" + m_dbname + "' --header 'Authorization: Token " + m_dbaccount + ":" + m_dbpassword + "'  --data-binary '";
+            querry = "curl -i -XPOST '"+ m_postProtocol +"://" + m_host + ":" + m_port + "/write?db=" + m_dbname + "' --header 'Authorization: Token " + m_dbaccount + ":" + m_dbpassword + "'  --data-binary '";
             
             for (unsigned long int i = 0; i < insertsVector.size(); i++)
             {
@@ -107,9 +114,10 @@ namespace dunedaq::influxopmon {
         std::string m_dbname;
         std::string m_dbaccount;
         std::string m_dbpassword;
+        std::string m_postProtocol;
         
         std::vector<std::string> tagSetVector;
-        std::string timeVariableName = ".time=";
+        std::string m_delimiter;
         std::vector<std::string> insertsVector;
         
         std::string querry;
