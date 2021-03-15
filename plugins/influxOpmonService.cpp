3ea6e55e7972873d81f8af45b350dd84ca0ba49f
Tenkarast <61792464+Tenkarast@users.noreply.github.com>
2021-03-15 19:58:36 +0100

    Update influxOpmonService.cpp

--- plugins/influxOpmonService.cpp
+++ plugins/influxOpmonService.cpp
@@ -43,7 +43,9 @@ namespace dunedaq::influxopmon {
             }
             return result;
         }
-
+        void executionCommandSilent(const char* cmd) {
+            system(cmd);
+        }
 
         explicit influxOpmonService(std::string uri) : dunedaq::opmonlib::OpmonService(uri) {
             uri = uri.substr(uri.find("/") + 2);
@@ -88,7 +90,7 @@ namespace dunedaq::influxopmon {
             querry = querry + "'";
 
             charPointer = querry.c_str();
-            executionCommand(charPointer);
+            executionCommandSilent(charPointer);
             //std::cout << executionCommand(charPointer);
         }
 
