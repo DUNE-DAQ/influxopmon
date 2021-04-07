1f77f813378ee4cdbc99fd7c3016d1336fbbabc7
Tenkarast <yann@donon.ch>
2021-04-07 15:42:17 +0200

    URI modification to reflect the use of a reverse proxy

--- plugins/influxOpmonService.cpp
+++ plugins/influxOpmonService.cpp
@@ -48,6 +48,7 @@ namespace dunedaq::influxopmon {
         }
 
         explicit influxOpmonService(std::string uri) : dunedaq::opmonlib::OpmonService(uri) {
+
             uri = uri.substr(uri.find("/") + 2);
             std::string tmp;
             std::stringstream ss(uri);
@@ -59,26 +60,23 @@ namespace dunedaq::influxopmon {
                 countArgs++;
             }
 
-            if (countArgs < 7)
+            if (countArgs < 3)
             {
-                std::cout << "invalid URI, follow: influx://Db-hostname:port:database:Username:Password:Protocol(http/https):Delimiter:Tags(0..N) \n Example: influx://dbod-testinfluxyd.cern.ch:8095:db1:usr:pwd:https:.time=:.class_name=\n";
+                std::cout << "invalid URI, follow: influx://proxyAdress:database:Delimiter:Tags(0..N) \n Example: influx://188.185.88.195:db1:.time=:.class_name=\n";
                 exit(0);
             }
 
-            //std::string uri = "influx://dbod-testinfluxyd.cern.ch:8095:db1:admin:admin:https:.time=:.class_name="
-            m_host = ressource[0];
-            m_port = ressource[1];
-            m_dbname = ressource[2];
-            m_dbaccount = ressource[3];
-            m_dbpassword = ressource[4];
-            m_postProtocol = ressource[5];
-            m_delimiter = ressource[6];
+	    m_host = ressource[0];
+            m_dbname = ressource[1];
+            m_delimiter = ressource[2];
 
-            for (unsigned long int i = 7; i < ressource.size(); i++)
+            for (unsigned long int i = 3; i < ressource.size(); i++)
             {
                 tagSetVector.push_back(ressource[i]);
             }
-        }
+
+
+	}
 
         void publish(nlohmann::json j)
         {
@@ -87,7 +85,7 @@ namespace dunedaq::influxopmon {
             insertsVector = jsonConverter.getInsertsVector();
             
             
-            querry = "curl -i -XPOST '"+ m_postProtocol +"://" + m_host + ":" + m_port + "/write?db=" + m_dbname + "' --header 'Authorization: Token " + m_dbaccount + ":" + m_dbpassword + "'  --data-binary '";
+            querry = "curl "  + m_host +  "/?db=" + m_dbname + " --data-binary '";
             
             for (unsigned long int i = 0; i < insertsVector.size(); i++)
             {
@@ -101,11 +99,12 @@ namespace dunedaq::influxopmon {
             executionCommand(charPointer);
 
             //output
-            /*
-            querry = querry + "'";
-            charPointer = querry.c_str();
+/*           
+    	    querry = querry + "\'";
+            std::cout << querry;
+	    charPointer = querry.c_str();
             std::cout << executionCommand(charPointer);
-            */
+  */          
             
         }
 
