67ef3b00327ae47a5e1f9000962c6ee71806c3cd
Tenkarast <yann@donon.ch>
2021-04-15 10:40:00 +0200

    added libcurl

--- plugins/influxOpmonService.cpp
+++ plugins/influxOpmonService.cpp
@@ -11,8 +11,8 @@
 #include <stdexcept>
 #include <array>
 #include "JsonInfluxConverter.hpp"
-
-
+//#include "cpr/cpr.h"
+#include <curl/curl.h>
 
 using json = nlohmann::json;
 
@@ -74,6 +74,45 @@ namespace dunedaq::influxopmon {
             {
                 tagSetVector.push_back(ressource[i]);
             }
+
+
+		std::cout << "\n curl code";
+
+  		CURL *curl;
+		CURLcode res;
+ 
+  		curl_global_init(CURL_GLOBAL_DEFAULT);
+
+		curl = curl_easy_init();
+	
+  		if(curl) {
+    			/* First set the URL that is about to receive our POST. This URL can
+ 			*        just as well be a https:// URL if that is what should receive the
+			 *               data. */ 
+    			curl_easy_setopt(curl, CURLOPT_URL, "http://postit.example.com/moo.cgi");
+    			/* Now specify the POST data */ 
+    			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
+ 
+    			/* Perform the request, res will get the return code */ 
+    			res = curl_easy_perform(curl);
+    			/* Check for errors */ 
+    			if(res != CURLE_OK)
+      				fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+ 
+		    	/* always cleanup */ 
+		  	curl_easy_cleanup(curl);
+  		  }	
+		  curl_global_cleanup();
+
+
+
+
+		
+
+	//	cpr::Response r = cpr::Get(cpr::Url{"https://google.com"});
+		std::cout << "\n end curl code\n\n";
+
+
 
 
 	}
